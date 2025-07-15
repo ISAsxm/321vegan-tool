69584d353b6a15cd9caa25729fb9f59d4e9bb7d0
ISAsxm <47738803+ISAsxm@users.noreply.github.com>
2025-07-15 15:44:39 -0400

    style: fix word wrap in table cell

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -36,6 +36,8 @@ const StyledHeader = styled(CommonRow)`
 
 const StyledRow = styled(CommonRow)`
   padding: 1.2rem 2.4rem;
+  word-wrap: break-word;
+  word-break: break-all;
 
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
