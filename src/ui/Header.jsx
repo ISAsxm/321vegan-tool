56eb0a9fbc5df05d1607ca20274630d7e100bfc4
llambrecht <louis.lambrecht@chargemap.com>
2025-12-29 23:11:00 +0100

    :sparkles: Add users stats

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,9 @@
 import HeaderMenu from "./HeaderMenu";
 
 import UserAvatar from "@/features/authentication/UserAvatar";
+import { useCurrentUserContext } from "@/contexts/CurrentUserContext";
 
+import { HiCheckBadge, HiOutlineEnvelope } from "react-icons/hi2";
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
@@ -14,9 +16,54 @@ const StyledHeader = styled.header`
   justify-content: flex-end;
 `;
 
+const UserStats = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  padding: 0.6rem 1.6rem;
+  background-color: var(--color-grey-50);
+  border-radius: var(--border-radius-sm);
+  font-size: 1.3rem;
+  color: var(--color-grey-600);
+`;
+
+const StatItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+
+  svg {
+    width: 1.6rem;
+    height: 1.6rem;
+    color: var(--color-brand-600);
+  }
+
+  span {
+    font-weight: 600;
+    color: var(--color-grey-700);
+  }
+`;
+
 function Header() {
+  const { currentUser } = useCurrentUserContext();
+  
+  const productsModified = currentUser?.nb_products_modified || 0;
+  const contactsMade = currentUser?.nb_checkings || 0;
+
   return (
     <StyledHeader>
+      <UserStats>
+        <StatItem>
+          <HiCheckBadge />
+          <span>{productsModified}</span>
+          <span style={{ fontWeight: 400 }}>produit{productsModified > 1 ? 's' : ''}</span>
+        </StatItem>
+        <StatItem>
+          <HiOutlineEnvelope />
+          <span>{contactsMade}</span>
+          <span style={{ fontWeight: 400 }}>contact{contactsMade > 1 ? 's' : ''}</span>
+        </StatItem>
+      </UserStats>
       <UserAvatar />
       <HeaderMenu />
     </StyledHeader>
